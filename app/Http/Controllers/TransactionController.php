1ec20e4d840389b49f05097439a99f6ec419b005
Tomasz Krzykawski <tkrzykawski@us.edu.pl>
2019-02-21 20:30:04 +0100

    remove update method from transaction ctr

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -132,16 +132,4 @@ class TransactionController extends Controller
 
         return response()->noContent();
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
 }
